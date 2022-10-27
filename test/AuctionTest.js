1f2f41eb1d0771465157f3b38ab165bd4ed8f9e9
Alina <307950@niuitmo.ru>
2022-10-27 22:32:28 +0300

    changed

--- test/AuctionTest.js
+++ test/AuctionTest.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
-describe("AucEngine", function () {
+describe("Auction", function () {
   let owner
   let seller
   let buyer
@@ -10,14 +10,9 @@ describe("AucEngine", function () {
   beforeEach(async function () {
     [owner, seller, buyer] = await ethers.getSigners()
 
-    const AucEngine = await ethers.getContractFactory("Auction", owner)
-    auct = await AucEngine.deploy()
+    const Auction = await ethers.getContractFactory("Auction", owner)
+    auct = await Auction.deploy()
     await auct.deployed()
-  })
-
-  it("sets owner", async function() {
-    const currentOwner = await auct.owner()
-    expect(currentOwner).to.eq(owner.address)
   })
 
   describe("createAuction", function () {
